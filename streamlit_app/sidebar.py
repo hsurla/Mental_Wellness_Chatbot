dc1e2a6372059eb605d1a96ab06f0758d0a857b5
Hemakesh Surla <143519486+hsurla@users.noreply.github.com>
2025-05-11 11:53:13 +0530

    123

--- streamlit_app/sidebar.py
+++ streamlit_app/sidebar.py
@@ -3,6 +3,6 @@ import streamlit as st
 def sidebar():
     with st.sidebar:
         st.title("Navigation")
-        page = st.radio("Go to", ["Chatbot", "Chat History", "Profile", "Logout"])
+        page = st.radio("Go to", ["Chatbot", "Wellness", "Chat History", "Profile", "Logout"])
 
     return page
