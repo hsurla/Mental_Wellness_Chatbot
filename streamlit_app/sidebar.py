cf5955e28d319484a995f8409632540aef078df3
Naga Jaswanth <nagajaswanth1054@gmail.com>
2025-05-11 14:42:47 +0530

    1

--- streamlit_app/sidebar.py
+++ streamlit_app/sidebar.py
@@ -6,9 +6,6 @@ def sidebar():
     with st.sidebar:
         st.title("Navigation")
         page = st.radio("Go to", ["Chatbot", "Wellness", "Chat History", "Profile", "Logout"])
-<<<<<<< HEAD
-=======
 
->>>>>>> dc1e2a6372059eb605d1a96ab06f0758d0a857b5
     return page
 
