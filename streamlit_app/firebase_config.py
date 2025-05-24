e41f502559579d71def69a1e2881cedf12d56600
Naga Jaswanth <nagajaswanth1054@gmail.com>
2025-05-24 17:18:29 +0530

    1

--- streamlit_app/firebase_config.py
+++ streamlit_app/firebase_config.py
@@ -1,12 +1,13 @@
 import pyrebase
 
-firebaseConfig = {
-  "apiKey": "YOUR_API_KEY",
-  "authDomain": "YOUR_PROJECT.firebaseapp.com",
-  "projectId": "YOUR_PROJECT_ID",
-  "storageBucket": "YOUR_PROJECT.appspot.com",
-  "messagingSenderId": "SENDER_ID",
-  "appId": "YOUR_APP_ID",
-  "measurementId": "G-XXXXXXX",
-  "databaseURL": ""  # Can be left blank if not using Realtime DB
-}
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyDiU7jEtrBeaUcEE6FkI9wNjSe5HZeDp3U",
+  authDomain: "mental-wellness-903df.firebaseapp.com",
+  projectId: "mental-wellness-903df",
+  storageBucket: "mental-wellness-903df.firebasestorage.app",
+  messagingSenderId: "760138862434",
+  appId: "1:760138862434:web:b9e74f445de7958aad663b",
+  measurementId: "G-JJ0ZLHBMNH"
+};
