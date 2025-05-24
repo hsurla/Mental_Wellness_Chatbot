d81b2fc6cf8eeb67a3f9c981c7a8bdf8f0a05437
Naga Jaswanth <nagajaswanth1054@gmail.com>
2025-05-24 14:47:13 +0530

    1

---
+++ streamlit_app/firebase_config.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  "apiKey": "YOUR_API_KEY",
+  "authDomain": "YOUR_PROJECT.firebaseapp.com",
+  "projectId": "YOUR_PROJECT_ID",
+  "storageBucket": "YOUR_PROJECT.appspot.com",
+  "messagingSenderId": "SENDER_ID",
+  "appId": "YOUR_APP_ID",
+  "measurementId": "G-XXXXXXX",
+  "databaseURL": ""  # Can be left blank if not using Realtime DB
+}
