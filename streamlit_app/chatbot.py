38d2977fc5d64c98e63c820f0e72d0f78154dd7a
Hemakesh Surla <143519486+hsurla@users.noreply.github.com>
2025-07-04 10:53:55 +0530

    h

--- streamlit_app/chatbot.py
+++ streamlit_app/chatbot.py
@@ -9,6 +9,7 @@ from streamlit_app.sidebar import sidebar
 from database.database import get_chat_history
 from streamlit_app.wellness import wellness_page
 from streamlit_app.profile import profile_page
+from streamlit_app.fun_support import get_fun_activity, get_healthy_snack
 
 # Lazy import to prevent circular imports
 def get_chatbot():
