d143f3faa21b5d61a61494d18f2690d79595d589
Naga Jaswanth <nagajaswanth1054@gmail.com>
2025-07-09 13:40:40 +0530

    1

--- streamlit_app/login.py
+++ streamlit_app/login.py
@@ -4,8 +4,11 @@ from google_auth_oauthlib.flow import Flow
 import requests
 
 # Configuration (REPLACE WITH YOURS)
-CLIENT_ID = "your-client-id.apps.googleusercontent.com"
-CLIENT_SECRET = "your-client-secret"
+CLIENT_ID = "95879444252-
+71052beum9527nbj32qbcan2h8i1caan.apps.googleusercontent.com"
+CLIENT_SECRET = "GOCSPX-
+
+1_6TTdSSLSc7wknZX5V7nRIDbPWK"
 REDIRECT_URI = "http://localhost:8501"  # Must match Google Console
 
 def login_page():
